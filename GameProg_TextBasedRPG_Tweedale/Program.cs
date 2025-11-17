5172a0d7dfcc936f4c79a2dd634f203900d62eb8
Michael <hubu99@gmail.com>
2025-11-17 11:16:29 -0400

    Map .txt File and Player Input
    Repurposing this old code for the "First Playable" version of this assignment.
    
    - Made it so the map is now read from a .txt file and saved in a string array
    - Added player input and draws the player on screen
    - Player input checks if the ground is not grass and doesn't move if so
    - Rather than waiting for input, the console checks if theres an available input and then reads it if so, and loops regardless. This will allow enemies to move even when the player isn't so the app feels more real-time.

--- GameProg_TextBasedRPG_Tweedale/Program.cs
+++ GameProg_TextBasedRPG_Tweedale/Program.cs
@@ -1,36 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameProg_TextBasedRPG_Tweedale
 {
+    internal class Player 
+    {
+        private int maxHP = 5;
+        private int health;
+        private int attack;
+        public int xPos;
+        public int yPos;
+
+        public Player(int hp, int att, int x, int y) 
+        {
+            health = hp; 
+            attack = att; 
+            xPos = x; 
+            yPos = y;
+        }
+
+        public int GetHealth() { return health; }
+        public int GetAttack() { return attack; }
+        public int XPos() { return xPos; }
+        public int YPos() { return yPos; }
+
+        public void Heal(int heal) 
+        {
+            health = Math.Min(health + heal, maxHP);
+        }
+
+        public void TakeDamage(int dmg) 
+        {
+            health = Math.Max(health - dmg, 0);
+        }
+
+        public void MoveHorizontal(int movement) { xPos = xPos + movement; }
+        public void MoveVertical(int movement) { yPos = yPos + movement; }
+        public void SetPosition((int, int) pos) 
+        {
+            yPos = pos.Item1;
+            xPos = pos.Item2;
+        }
+    }
+
     internal class Program
     {
-        static char[,] map = new char[,] // dimensions defined by following data:
+        static int scale = 1;
+        static string[] map;
+
+        static string[] testMap = new string[] // dimensions defined by following data:
         {
-            {'^','^','^','`','`','`','`','`','`','`','`','`','`','*','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^'},
-            {'^','^','`','`','`','`','*','*','`','`','`','`','`','`','*','`','`','`','`','`','`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','^','^'},
-            {'^','^','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','~','~','~','`','`','`','`','`','`','`','^','^','^'},
-            {'^','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','^','^','^','^','^','^'},
-            {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','*','`','`','`','`','`','`','`','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~'},
-            {'`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','*','*','*','`','`','`','`','`','`','`','*','*','*','`','`','`','^','^','^','^','^','^','^'},
-            {'`','`','`','~','~','~','~','`','`','`','`','`','`','`','*','*','`','`','`','`','*','*','^','^','`','`','*','`','`','`','`','`','`','^','^','^','^','^'},
-            {'`','`','`','`','`','~','~','~','`','`','`','`','`','`','`','`','`','`','`','*','*','^','^','^','^','`','`','`','`','`','`','`','`','`','^','^','^','^'},
-            {'*','`','`','`','`','~','~','~','~','`','`','`','`','`','`','`','*','`','`','`','*','*','*','^','^','^','^','`','`','`','*','*','`','`','`','^','^','^'},
-            {'*','`','`','`','`','`','`','~','`','`','`','`','`','`','`','`','*','*','*','`','`','`','*','*','`','`','`','`','`','`','*','`','`','`','`','`','`','^'},
-            {'*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^'},
-            {'*','*','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','^','^','^','^'},
+            "^^", 
+            "$@"
         };
 
-        static char[,] testMap = new char[,] // dimensions defined by following data:
-        {
-            {'^', '^' },
-            {'$', '@' }
-        };
-
-        
+        static Player player;
 
         // usage: map[y, x]
 
@@ -45,58 +74,147 @@ namespace GameProg_TextBasedRPG_Tweedale
 
         static void Main(string[] args)
         {
-            int scale = 1;
+            Console.CursorVisible = false;
             bool looping = true;
+            ReadMap();
 
-            //PrintMap(testMap, 1);
-            //PrintMap(testMap, 4);
+            player = new Player(5, 1, 0, 0);
+            player.SetPosition(GetStartPosition());
+            
             DisplayMap();
+            DrawPlayer();
 
             while (looping) 
             {
-                Console.WriteLine();
-                if (scale > 1) Console.Write($"{scale - 1} <--");
-                else Console.Write("     ");
-                Console.Write($"    {scale}    ");
-                Console.WriteLine($"--> {scale + 1}");
-                Console.WriteLine("\n     Esc = Quit");
-
-                bool readinginput = true;
-
-                while (readinginput) 
-                {
-                    ConsoleKey input = Console.ReadKey(true).Key;
-
-                    if (input == ConsoleKey.LeftArrow && scale > 1)
-                    {
-                        scale--;
-                        readinginput = false;
-                    }
-                    else if (input == ConsoleKey.RightArrow)
-                    {
-                        scale++;
-                        readinginput = false;
-                    }
-                    else if (input == ConsoleKey.Escape) 
-                    {
-                        readinginput = false;
-                        looping = false;
-                        Console.WriteLine("Goodbye!");
-                    }
-                }
-
-                if (looping) 
-                {
-                    Console.Clear();
-                    DisplayMap(scale);
-                }
-
+                Thread.Sleep(300);
+                ReadPlayerInput();
+                DrawPlayer();
             }
+        }
+
+        static void ReadPlayerInput() 
+        {
+            if (Console.KeyAvailable) 
+            {
+                int px = player.XPos();
+                int py = player.YPos();
+
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                DrawPlayer(true);
+
+                if (input == ConsoleKey.W || input == ConsoleKey.UpArrow) 
+                {
+                    if (py > 0) 
+                    {
+                        if (map[py - 1][px] == '`' || map[py - 1][px] == '_') 
+                        {
+                            player.MoveVertical(-1);
+                        }
+                    }
+                }
+                else if (input == ConsoleKey.S || input == ConsoleKey.DownArrow)
+                {
+                    if (py < map.GetLength(0))
+                    {
+                        if (map[py + 1][px] == '`' || map[py - 1][px] == '_')
+                        {
+                            player.MoveVertical(1);
+                        }
+                    }
+                }
+                else if (input == ConsoleKey.A || input == ConsoleKey.LeftArrow)
+                {
+                    if (px > 0)
+                    {
+                        if (map[py][px-1] == '`' || map[py][px - 1] == '_')
+                        {
+                            player.MoveHorizontal(-1);
+                        }
+                    }
+                }
+                else if (input == ConsoleKey.D || input == ConsoleKey.RightArrow)
+                {
+                    if (px < map[0].Length)
+                    {
+                        if (map[py][px + 1] == '`' || map[py][px + 1] == '_')
+                        {
+                            player.MoveHorizontal(1);
+                        }
+                    }
+                }
+
+                while (Console.KeyAvailable)
+                {
+                    Console.ReadKey(false);
+                }
+            }
+        }
+
+        static bool OldMapScalingCode() 
+        {
+            //DisplayLegend();
+            Console.WriteLine();
+            if (scale > 1) Console.Write($"{scale - 1} <--");
+            else Console.Write("     ");
+            Console.Write($"    {scale}    ");
+            Console.WriteLine($"--> {scale + 1}");
+            Console.WriteLine("\n     Esc = Quit");
+
+            bool readinginput = true;
+
+            while (readinginput)
+            {
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                if (input == ConsoleKey.LeftArrow && scale > 1)
+                {
+                    scale--;
+                    readinginput = false;
+                }
+                else if (input == ConsoleKey.RightArrow)
+                {
+                    scale++;
+                    readinginput = false;
+                }
+                else if (input == ConsoleKey.Escape)
+                {
+                    readinginput = false;
+                    return false;
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+
+            Console.Clear();
+            DisplayMap(scale);
+            DisplayLegend();
+            return true;
+        }
+
+        static void DrawPlayer(bool invisible = false) 
+        {
+            Console.SetCursorPosition(player.XPos()+1, player.YPos()+1);
+
+            GetColorForChar(map[player.YPos()][player.XPos()]);
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            if (!invisible) Console.Write('Ö');
+            else Console.Write(' ');
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void ReadMap() 
+        {
+            string path = @"map.txt"; // Get the path of the file as a string
+
+            map = File.ReadAllLines(path); // Read the data from that file path and save it as a string array (where each line is a separate string in the array)
         }
 
         static void DisplayMap(int scale) 
         {
-            DrawHorizontalBorder(map.GetLength(1) * scale, true);
+            DrawHorizontalBorder(map[0].Length * scale, true);
 
             for (int row = 0; row < map.GetLength(0); row++) 
             {
@@ -104,54 +222,36 @@ namespace GameProg_TextBasedRPG_Tweedale
                 {
                     Console.Write('║');
                     
-                    for (int col = 0; col < map.GetLength(1); col++)
+                    for (int col = 0; col < map[0].Length; col++)
                     {
                         for (int dupeCol = 0; dupeCol < scale; dupeCol++)
                         {
-                            char c = map[row, col];
+                            char c = '#';
+
+                            if (map[row].Length > col) 
+                            {
+                                c = map[row][col];
+                            }
 
                             GetColorForChar(c);
 
-                            Console.Write(map[row, col]);
+                            Console.Write(' ');
 
                             Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.Black;
                         }
                     }
                         
                     Console.WriteLine('║');
                 }
-                
             }
-            DrawHorizontalBorder(map.GetLength(1) * scale, false);
-            DisplayLegend();
+            DrawHorizontalBorder(map[0].Length * scale, false);
+            
         }
 
         static void DisplayMap()
         {
-            DrawHorizontalBorder(map.GetLength(1), true);
-
-            for (int row = 0; row < map.GetLength(0); row++)
-            {
-                Console.Write('║');
-
-                for (int col = 0; col < map.GetLength(1); col++)
-                {
-                    char c = map[row, col];
-
-                    GetColorForChar(c);
-
-                    Console.Write(map[row, col]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-                        
-                }
-
-                Console.WriteLine('║');
-                
-
-            }
-            DrawHorizontalBorder(map.GetLength(1), false);
-            DisplayLegend();
+            DisplayMap(1);
         }
 
         static void DrawHorizontalBorder(int length, bool top) 
@@ -189,11 +289,39 @@ namespace GameProg_TextBasedRPG_Tweedale
 
         static void GetColorForChar(char c) 
         {
-            if (c == '`') Console.ForegroundColor = ConsoleColor.Green;
-            else if (c == '~') Console.ForegroundColor = ConsoleColor.Blue;
-            else if (c == '^') Console.ForegroundColor = ConsoleColor.DarkGray;
-            else if (c == '*') Console.ForegroundColor = ConsoleColor.DarkGreen;
-            else Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            if (c == '`') Console.BackgroundColor = ConsoleColor.Green;
+            else if (c == '~') Console.BackgroundColor = ConsoleColor.Blue;
+            else if (c == '^') Console.BackgroundColor = ConsoleColor.DarkGray;
+            else if (c == '*') Console.BackgroundColor = ConsoleColor.DarkGreen;
+            else if (c == '#') Console.BackgroundColor = ConsoleColor.Magenta;
+            else if (c == '_') Console.BackgroundColor = ConsoleColor.Green;
+            else Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        static (int, int) GetStartPosition() 
+        {
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int col = 0; col < map[0].Length; col++)
+                {  
+                    char c = '#';
+
+                    if (map[row].Length > col)
+                    {
+                        c = map[row][col];
+                    }
+
+                    if (c == '_') 
+                    {
+                        return (row, col);
+                    }
+                      
+                }
+            }
+
+            return (0, 0);
         }
     }
 }
